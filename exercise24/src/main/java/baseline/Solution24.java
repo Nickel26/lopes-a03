a38d519999fa5b790b8dd89093e1e44a1511cf1e
Nick <lopes.nick0801@gmail.com>
2021-10-01 00:55:28 -0400

    code written

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -4,35 +4,57 @@
  */
 package baseline;
         import java.util.Scanner;
+        import java.util.Arrays;
 
 public class Solution24 {
+    private static final Scanner in = new Scanner(System.in);
     public static void main(String[] args){
         Solution24 app = new Solution24();
         //Declare two strings
         String str1;
         String str2;
-        System.out.print("Enter two strings and I'll tell you if they are anagrams: ");
+        System.out.print("Enter two strings and I'll tell you if they are anagrams: \n");
         //Prompt user for first string
         str1 = app.getStringFromUser();
         //Prompt user for second string
         str2 = app.getStringFromUser();
-        //Determine if two strings are anagrams
-        int result = app.isAnagram(str1, str2);
-
-        //Print out result
-
+        //Determine if two strings are anagrams and print result
+        if(app.isAnagram(str1, str2)){
+            System.out.print(str1 + " and "+ str2 +" are anagrams.");
+        }
+        else
+            System.out.print(str1 + " and "+ str2 +" are not anagrams.");
     }
 
     private String getStringFromUser() {
         //print input prompt
-        //take in string
-        //return new string
-        return null;
+        System.out.print("Enter a string: ");
+
+        //take in string and return
+        return in.next();
     }
 
-    private int isAnagram(String str1, String str2) {
-        //check number of letters and each letter to determine if strings are anagrams
-        return 0;
+    private boolean isAnagram(String str1, String str2) {
+        //check string length and each letter to determine if strings are anagrams
+        int n1 = str1.length();
+        int n2 = str2.length();
+
+
+        if(n1 != n2){
+            return false;
+        }
+        else{
+            char[] s1 = str1.toCharArray();
+            Arrays.sort(s1);
+
+            char[] s2 = str2.toCharArray();
+            Arrays.sort(s2);
+            if(Arrays.equals(s1, s2))
+                return true;
+            else
+                return false;
+        }
+
     }
 
 
