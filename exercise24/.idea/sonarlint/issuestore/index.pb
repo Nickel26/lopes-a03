eb14cbb697eb799ed9f64642a12b6454f9a5afd3
Nick <lopes.nick0801@gmail.com>
2021-10-02 00:01:34 -0400

    Skeleton code

---
+++ exercise24/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+V
+&src/main/java/baseline/Solution24.java,6\a\6a3518ac797c1fdae3479160a6036bf1929dcb92
+Z
+*src/test/java/baseline/Solution24Test.java,b\8\b8c7d6a717f0fad091fd8f900ccb0ace26334cf5
--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -1,2 +1,29 @@
-package baseline;public class Solution25 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution25 {
+    public static void main(String[] args){
+        //initialize two strings
+        int strength;
+        String pass[];
+        //prompt user for password
+        //give password to checkStrength
+        strength = passwordValidator(pass);
+        //give strength to printStrength function to display password strength
+        printStrength(strength);
+
+    }
+
+    private static void printStrength(int strength) {
+        //check password strength value to determine which prompt to print
+    }
+
+    private static int passwordValidator(String pass[]) {
+        //check password features with standard values
+        return 0;
+    }
 }
