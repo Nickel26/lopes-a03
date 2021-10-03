18f53161a24a788ae0a438b450fe7724164a54f8
Nick <lopes.nick0801@gmail.com>
2021-10-03 18:21:02 -0400

    Skeleton Code

---
+++ exercise30/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution30.java,3\9\39a521ca6004d2de0a7f6aab8bcf1656a9905e2f
---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution31 {
+    private static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+        //take input for age and resting pulse
+        //check to make sure inputs are numerical values
+        //pass inputs to calculateHeartRate method
+        calculateHeartRate();
+    }
+
+    private static void calculateHeartRate() {
+        //apply age and resting pulse to formula
+        //display calculated heart rate with each increased intensity
+    }
+}
