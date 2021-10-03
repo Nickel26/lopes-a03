7e2dcdf76dc0440a58ed94d7f4410af3f11b92b5
Nick <lopes.nick0801@gmail.com>
2021-10-03 19:08:04 -0400

    Skeleton Code

---
+++ exercise31/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+Z
+*src/test/java/baseline/Solution31Test.java,8\0\80e0cf5529fe6ad60c2678b057ceab186ff7d4f3
+V
+&src/main/java/baseline/Solution31.java,5\c\5cafb99e82926136c2f1d044c79185ad9c24e74e
---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution32 {
+    private static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+        
+        //prompt user for desired difficulty level
+        //depending on user choice call selected method
+        numGame1();
+        numGame2();
+        numGame3();
+        //ask user if they wish to play again
+    }
+
+    private static void numGame3() {
+        //picks random number between 1 and 1000
+        //prompt user to guess number
+        //display if user is higher or lower than number
+        //display how many guesses user took
+    }
+
+    private static void numGame2() {
+        //picks random number between 1 and 100
+        //prompt user to guess number
+        //display if user is higher or lower than number
+        //display how many guesses user took
+    }
+
+    private static void numGame1() {
+        //picks random number between 1 and 10
+        //prompt user to guess number
+        //display if user is higher or lower than number
+        //display how many guesses user took
+    }
+}
