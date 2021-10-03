23ae25bb44d301ed4900301c82cbffd89687038d
Nick <lopes.nick0801@gmail.com>
2021-10-03 18:55:49 -0400

    Solution31

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -8,14 +8,49 @@ import java.util.Scanner;
 public class Solution31 {
     private static final Scanner in = new Scanner(System.in);
     public static void main(String[] args){
+        String age, restingPulse;
+
         //take input for age and resting pulse
-        //check to make sure inputs are numerical values
+        while(true) {
+            System.out.print("What is the resting pulse? ");
+            restingPulse = in.next();
+
+            //check to make sure inputs are numerical values
+            if(restingPulse.chars().allMatch( Character::isDigit )) {
+                while(true) {
+                    System.out.print("What is the age? ");
+                    age = in.next();
+                    if (age.chars().allMatch(Character::isDigit)) {
+                        break;
+                    }
+                    else
+                        System.out.print("Please enter a numeric value. \n");
+
+                }
+                break;
+            }
+            else
+                System.out.print("Please enter a numeric value. \n");
+        }
+
         //pass inputs to calculateHeartRate method
-        calculateHeartRate();
+        calculateHeartRate(Double.parseDouble(age), Double.parseDouble(restingPulse));
     }
 
-    private static void calculateHeartRate() {
+    private static void calculateHeartRate(double age, double restingPulse) {
+        double TargetHeartRate = 0;
+        //display output format
+        System.out.printf("\nResting Pulse: %.0f \t Age: %.0f \n\nIntensity\t | Rate \n-------------|--------\n", restingPulse, age);
         //apply age and resting pulse to formula
+        for(double i = 0.55; i <= 0.96; i += 0.05){
+            TargetHeartRate = karvonenHeartRate(age, restingPulse, i);
+            System.out.printf("%.0f\t\t\t | %.0f\n", (i * 100), TargetHeartRate);
+        }
         //display calculated heart rate with each increased intensity
+    }
+
+    public static double karvonenHeartRate(double age, double restingPulse, double i) {
+        double heartRate = Math.round((((220.0 - age) - restingPulse) * i) +restingPulse);
+        return heartRate;
     }
 }
--- exercise31/src/test/java/baseline/Solution31Test.java
+++ exercise31/src/test/java/baseline/Solution31Test.java
@@ -1,4 +1,23 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Solution31Test {
-  
+
+    @Test
+    void main() {
+        Solution31 app = new Solution31();
+
+        double expectedHeartRate = 151;
+        double actual;
+        double i = 0.65;
+        double age = 22;
+        double restingPulse = 65;
+
+        actual = app.karvonenHeartRate(age, restingPulse, i);
+        assertEquals(expectedHeartRate, actual);
+
+    }
 }
