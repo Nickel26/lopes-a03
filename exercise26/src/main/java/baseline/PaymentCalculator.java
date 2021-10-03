0e0adea5345bb259120ab1cb03e57ed5ef7d8a38
Nick <lopes.nick0801@gmail.com>
2021-10-03 00:26:48 -0400

    Skeleton code

--- exercise26/src/main/java/baseline/PaymentCalculator.java
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -1,2 +1,13 @@
-package baseline;public class PaymentCalculator {
+package baseline;
+
+public class PaymentCalculator {
+
+    public static int calculateMonthsUntilPaidOff() {
+        //calculates how many months until card is paid off using formula
+        int n;
+
+
+        return n;
+    }
+
 }
--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -1,2 +1,22 @@
-package baseline;public class Solution26 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution26 {
+    private static final Scanner in = new Scanner(System.in);
+    public void main(String[] args){
+        //initialize variables
+        int n, i, b, p;
+        //prompt user for balance on card, apr, and monthly payments
+
+        //pass information into Payment Calculator
+        n = PaymentCalculator.calculateMonthsUntilPaidOff();
+        //display number of months till card is paid off
+
+    }
+
+
 }
