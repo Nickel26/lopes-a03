14b94792a58c82a6d93ead80046773176ce5f881
Nick <lopes.nick0801@gmail.com>
2021-10-03 17:59:27 -0400

    Skeleton Code

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -1,2 +1,14 @@
-package baseline;public class Solution30 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution30 {
+    public static void main(String[] args){
+        //create a loop that iterates 12 times
+        //create a nested loop that also iterates 12 times
+        //inside nested loop multiply the first loop by the iteration of the nested and display that number
+    }
 }
