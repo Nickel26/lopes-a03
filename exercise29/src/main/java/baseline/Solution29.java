9736d78f328a882d1e07f027428324afa804a730
Nick <lopes.nick0801@gmail.com>
2021-10-03 16:19:16 -0400

    Skeleton code

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -1,2 +1,18 @@
-package baseline;public class Solution29 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution29 {
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //initialize variables
+        //loop if input is non numeric or zero until user gives numeric value
+        //pass rate of return to howManyYears method
+        //display number of years to double initial investment
+
+    }
 }
