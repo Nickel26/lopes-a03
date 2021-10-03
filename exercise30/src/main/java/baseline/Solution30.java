99484ff2ae206d5e3cffe5ee63610a2d03b9f77a
Nick <lopes.nick0801@gmail.com>
2021-10-03 18:08:34 -0400

    Solution30

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -8,6 +8,12 @@ import java.util.Scanner;
 public class Solution30 {
     public static void main(String[] args){
         //create a loop that iterates 12 times
+        for(int i = 1; i <= 12; i++){
+            for(int j = 1; j <= 12; j++){
+                System.out.format("%4d ", (i * j));
+            }
+            System.out.print("\n");
+        }
         //create a nested loop that also iterates 12 times
         //inside nested loop multiply the first loop by the iteration of the nested and display that number
     }
