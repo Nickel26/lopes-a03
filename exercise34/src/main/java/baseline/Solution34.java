694e9978e76d3106d08dcae0e6fc0eb5e8509259
Nick <lopes.nick0801@gmail.com>
2021-10-03 23:51:26 -0400

    Skeleton Code

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution34 {
+    //create array list with employee names
+    //Display full array list
+    //ask user for which employee to remove
+    //move all other employees into new arraylist
+    //display new arraylist
+}
